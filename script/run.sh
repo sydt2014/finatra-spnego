e6a7414969b4e90aac003bbed09d64277afa7eec
glegoux <gilles.legoux@gmail.com>
2018-06-24 22:14:21 +0200

    Improve run.sh

--- script/run.sh
+++ script/run.sh
@@ -1,6 +1,19 @@
 #!/usr/bin/env bash
 
+cd $(dirname "$0")
+
 debug=true
+
+jar_file_location="$1"
+shift
+
+jar_dir=$(dirname "${jar_file_location}")
+jar_name=$(basename "${jar_file_location}")
+
+cd "${jar_dir}"
+
+unzip "${jar_name}" jaas-krb5.conf
+unzip "${jar_name}" krb5.conf
 
 if [[ "${debug}" == "true" ]]; then
   export KRB5_TRACE=/dev/stderr
@@ -12,4 +25,4 @@ java \
   -Djavax.security.auth.useSubjectCredsOnly=false \
   -Dsun.security.spnego.debug="${debug}" \
   -Dsun.security.krb5.debug="${debug}" \
-  -jar $@
+  -jar "${jar_name}" $@
